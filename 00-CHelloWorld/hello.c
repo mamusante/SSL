5c395d0ae1a002530aa08b76f00644fead406c94
Maria Musante <mmusante@frba.utn.edu.ar>
2024-04-01 20:21:51 -0300

    Agregue header al hello.c

--- 00-CHelloWorld/hello.c
+++ 00-CHelloWorld/hello.c
@@ -1,3 +1,9 @@
+/* Qué: TP0 - Hello World
+ * Instalacion de compilador de c, manejo de github, salida a un archivo de un hello world
+ * Maria Musante (TP individual)
+ * 20240401
+ */
+
 #include <stdio.h>
 int main() {
    FILE* f = fopen("../output.txt", "w+");
